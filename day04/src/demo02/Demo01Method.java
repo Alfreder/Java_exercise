a969177e201f8885548a63a0dcea055292014603
tendanger <tendanger@outlook.com>
2019-01-06 03:41:16 +0800

    change Method

--- day04/src/demo02/Demo01Method.java
+++ day04/src/demo02/Demo01Method.java
@@ -19,12 +19,13 @@ public static void 方法名称() {
 public class Demo01Method {
 
     public static void main(String[] args) {
+
         printMethod();
     }
 
     public static void printMethod() {
         for (int j = 0; j < 5; j++) {
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < 10; i++) {
                 System.out.print("*");
             }
             System.out.println();
