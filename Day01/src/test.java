9ccbedd660c4eb8a9aec7bff6886c76d446a5b8e
Alfreder <tendanger@gmail.com>
2020-02-12 20:30:52 +0800

    change computer aaa

---
+++ Day01/src/test.java
@@ -0,0 +1,16 @@
+public class test {
+    public static void main(String[] args) {
+        //String s= new String("123");
+        String[]array=new String[10];
+        for(int i=0;i<array.length;i++){
+            array[i]= "1";
+        }
+        for (int j = 0; j <array.length; j++) {
+            System.out.println(array[j]);
+
+
+
+        }
+
+    }
+}
